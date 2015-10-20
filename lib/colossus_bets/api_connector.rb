27461be3eb9cbce8591b8d8d838d2fe926be2f9a
Ner0on <vadim05@inbox.ru>
2015-10-20 15:08:11 +0300

    error explain

--- lib/colossus_bets/api_connector.rb
+++ lib/colossus_bets/api_connector.rb
@@ -14,7 +14,11 @@ class ColossusBets::ApiConnector
   end
 
   def create_session
-    self.class.post("/v2/sessions/", {query: {api_key: 'KklyUtXD7NQb8oIYQDMBDw', api_secret: 'password' }} )
+    self.class.post("/v2/sessions/", {body: {api_key: 'KklyUtXD7NQb8oIYQDMBDw', api_secret: 'password' }} )
   end
+
+  # Main problem of this Test task is that API all the time send me 406 error and some of this "x-xss-protection"=>["1; mode=block”]. 
+  # It happens when i try to POST /v2/sessions using HTTPparty gem
+  # I tried to rename body to query (i read this from stockower. solutions ) but it doesn't helps me 
 
 end
